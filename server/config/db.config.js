a58d633cba40ff40077cc6be4708d2505f4c9ff3
nageshpatil-spec <nagesh.patil@dharamcs.com>
2025-12-10 17:49:45 +0530

    Configured supabase db

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,27 @@
-const config = { 
-	USER: "postgres",
-    HOST: "192.168.1.105",
-    DATABASE: "DGX",
-	PASSWORD: "root",
-    DIALECT: "postgres",
-    PORT: "7894"
-} 
+// const config = {
+// 	USER: "postgres",
+//     HOST: "192.168.1.105",
+//     DATABASE: "DGX",
+// 	PASSWORD: "root",
+//     DIALECT: "postgres",
+//     PORT: "7894"
+// }
+
+// module.exports = config;
+
+const config = {
+  USER: "postgres",
+  HOST: "db.mascmgiqcqezokumcamk.supabase.co",
+  DATABASE: "postgres",
+  PASSWORD: "NLhm9kMNCBOTi5hC",
+  DIALECT: "postgres",
+  PORT: 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+};
 
 module.exports = config;
