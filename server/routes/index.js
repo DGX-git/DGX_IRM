3b162d84b0f2bc2aa2a9e540aea9834b43b52739
Simran Jamal <simran.jamal@dharamcs.com>
2025-12-12 12:36:15 +0530

    fixed index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,9 +8,9 @@ router.use(cors());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // router.get('/getCpu', instancerequest.instancerequest);
-  // res.render('index', { title: 'Express' });
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+  router.get('/getCpu', instancerequest.instancerequest);
+  res.render('index', { title: 'Express' });
+    // res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 module.exports = router;
