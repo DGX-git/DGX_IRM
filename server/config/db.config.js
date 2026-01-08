496fb8dddad732f3505a5613b50b1ed87cba0778
Siddharth dharamcs <siddharth.kanbargi@dharamcs.com>
2026-01-08 15:36:12 +0530

    UAT build related config changes.

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -11,28 +11,28 @@
 // module.exports = config;
 
 // QA
-const config = {
-	USER: "postgres",
-    HOST: "192.168.1.105" ,
-    DATABASE: "qa_dgx",
-	PASSWORD: "root",
-    DIALECT: "postgres",
-    PORT: "7894"
-}
+// const config = {
+// 	USER: "postgres",
+//     HOST: "localhost" ,
+//     DATABASE: "qa_dgx",
+// 	PASSWORD: "root",
+//     DIALECT: "postgres",
+//     PORT: "7894"
+// }
 
-module.exports = config;
+// module.exports = config;
 
 
 
 
 // UAT
-// const config = {
-//     USER: "postgres",
-//     HOST: "localhost",
-//     DATABASE: "dgx_uat",
-//     PASSWORD: "root",
-//     DIALECT: "postgres",
-//     PORT: "5432"
-// }
+const config = {
+    USER: "postgres",
+    HOST: "localhost",
+    DATABASE: "dgx_uat",
+    PASSWORD: "root",
+    DIALECT: "postgres",
+    PORT: "5432"
+}
 
-// module.exports = config;
+module.exports = config;
